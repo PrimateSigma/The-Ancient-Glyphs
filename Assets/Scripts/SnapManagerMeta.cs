b3eaa6a12093f7521ba7155a64eae4d4c72c19fc
MikaelSyversson <113528776+Msyversson@users.noreply.github.com>
2025-03-03 20:14:49 +0100

    Puzzle nr2 done

---
+++ Assets/Scripts/SnapManagerMeta.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class SnapManagerMeta : MonoBehaviour
+{
+    public Transform[] snapPoints;
+    public GameObject[] snappableObjects;
+    public Animator targetAnimator;
+
+    private void Update()
+    {
+        if (AllObjectsSnapped())
+        {
+            targetAnimator.SetTrigger("PlayAnimation"); // Triggar animationen
+        }
+    }
+
+    private bool AllObjectsSnapped()
+    {
+        for (int i = 0; i < snappableObjects.Length; i++)
+        {
+            float distance = Vector3.Distance(snappableObjects[i].transform.position, snapPoints[i].position);
+            if (distance > 0.05f) // Om objektet inte är nära sin snap-punkt
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
